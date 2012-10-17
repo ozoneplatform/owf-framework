ff09dde6f9ad6d039ed42bc0ad979923d5ba1ec2
Kyle Yerty <kyle.yerty@nextcentury.com>
2012-10-17 16:24:03 -0400

    OWF-5804: As an Administrator, I would like to be able to create, edit, delete, and reorder stacks.
    1. Added barebones stack tab to all the appropriate editor widgets (won't work until stack services/controllers are committed and it is connected to them).

---
+++ web-app/js/components/admin/group/GroupEditStacksTab.js
@@ -0,0 +1,47 @@
+Ext.define('Ozone.components.admin.group.GroupEditStacksTab', {
+  extend: 'Ozone.components.admin.StacksTabPanel',
+  alias: ['widget.groupeditstacks',
+    'widget.groupeditstackstab',
+    'widget.Ozone.components.admin.group.GroupEditStacksTab'],
+
+  cls: 'groupeditstackstab',
+
+  initComponent: function () {
+    Ext.apply(this, {
+      padding: 5,
+      iconCls: 'stacks-tab',
+      editor: 'Group',
+      componentId: 'group_id',
+      title: 'Stacks',
+      storeCfg: {
+                api: {
+                    read: '/stack',
+                    create: '/group',
+                    update: '/stack',
+                    destroy: '/group'
+                },
+                methods: {
+                    read: 'GET',
+                    load: 'GET',
+                    create: 'PUT',
+                    update: 'PUT',
+                    save: 'POST',
+                    destroy: 'PUT'
+                },
+                updateActions: {
+                    destroy: 'remove',
+                    create: 'add'
+                },
+                pageSize: 50
+            }
+    });
+    this.callParent(arguments);
+  },
+  initBaseParams: function(record) {
+    this.baseParams = {
+      group_id: record.data.id
+    };
+    this.applyFilter();
+  }
+
+});
