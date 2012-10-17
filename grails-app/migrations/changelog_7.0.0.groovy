ea62ecf95661060e429f8e25ccc55ad8b5a7afbb
Greg Traub <gtraub@nuwavesolutions.com>
2012-10-17 13:01:37 -0400

    OWF-6298: Develop stack domain object

--- grails-app/migrations/changelog_7.0.0.groovy
+++ grails-app/migrations/changelog_7.0.0.groovy
@@ -54,6 +54,9 @@ databaseChangeLog = {
             column(autoIncrement: "true", name: "id", type: "bigint") {
                 constraints(nullable: "false", primaryKey: "true", primaryKeyName: "stackPK")
             }
+            column(name: "version", type: "bigint") {
+                constraints(nullable: "false")
+            }
             column(name: "name", type:"varchar(200)") {
                 constraints(nullable: "false")
             }
