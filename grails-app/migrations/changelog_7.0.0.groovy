70e0ac189dfa4359d888dfe3f885d1ea7c2026d7
Lawerence Mize <lawerence.mize@nextcentury.com>
2012-11-01 13:02:46 -0400

    OWF-6357: Adding dashboard support to stacks. Initial domain/service changes.

--- grails-app/migrations/changelog_7.0.0.groovy
+++ grails-app/migrations/changelog_7.0.0.groovy
@@ -120,4 +120,18 @@ databaseChangeLog = {
             column(name: "stack_default", type: "boolean", defaultValueBoolean: "false", valueBoolean: "false")
         }
     }
+    
+    changeSet(author: "owf", id: "7.0.0-15", context: "create, upgrade, 7.0.0") {
+        comment("Add a reference to a host stack to dashboard records to track where user instances should appear")
+        addColumn(tableName: "dashboard") {
+            column(name: "stack_id", type: "bigint")
+        }
+    }
+    
+    changeSet(author: "owf", id: "7.0.0-16", context: "create, upgrade, 7.0.0") {
+        comment("Add foreign key constraint for stack_id in the dashboard table")
+        addForeignKeyConstraint(constraintName: "FKC18AEA946B3A1281",
+            baseColumnNames: "stack_id", baseTableName: "dashboard", 
+            referencedColumnNames: "id", referencedTableName: "stack")
+    }
 }
