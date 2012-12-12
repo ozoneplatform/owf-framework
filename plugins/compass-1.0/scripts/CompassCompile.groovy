aa457fe28c482d49ab65070444b2caa755f65fe4
Nish Patel <nishantpatel611@gmail.com>
2012-12-12 09:39:36 -0500

    fix error: java.util.zip.ZipException: error in opening zip file
    AntClassLoader is going through each of the files in the classpath, the
    AntClassLoader.getResourceURL() method is called for each of them, and
    if the resource is a valid file, it attempts to open it as a ZipFile.
    This is a problem as not all files in compass-1.0/lib directory are zip
    files. Simply renaming it to libs and updating .groovy files in the
    plugin fixes this problem.

--- plugins/compass-1.0/scripts/CompassCompile.groovy
+++ plugins/compass-1.0/scripts/CompassCompile.groovy
@@ -12,7 +12,7 @@ target(compile:"Compile the directory") {
 
 public void compassPluginCompile() {
 	ant.path(id: 'jruby.classpath') { 
-		pathelement(location: "${compassPluginDir}/lib/jruby-complete-1.7.0.jar") 
+		pathelement(location: "${compassPluginDir}/libs/jruby-complete-1.7.0.jar") 
 	}
 	new File("web-app/themes").eachFile { file ->
 		if(file.isDirectory() && file.name.endsWith(".theme")) {
@@ -21,14 +21,12 @@ public void compassPluginCompile() {
 					fork:true, 
 					failonerror:true, 
 					classpathref: 'jruby.classpath',
-					dir: file
+					dir: new File(file.getPath() + '/sass')
 			) {
-					env(key: "GEM_HOME", value: "${compassPluginDir}/lib/gems")
+					env(key: "GEM_HOME", value: "${compassPluginDir}/libs/gems")
 					arg(value: "--1.8")
-					arg(value:"${compassPluginDir}/lib/gems/bin/compass")
+					arg(value:"${compassPluginDir}/libs/gems/bin/compass")
 					arg(value:"compile")
-					arg(value: "-c")
-					arg(value: "sass/config.rb")
 			}
 		}
 	}
--- plugins/compass-1.0/scripts/_Events.groovy
+++ plugins/compass-1.0/scripts/_Events.groovy
@@ -3,7 +3,7 @@ includeTargets << new File("${compassPluginDir}/scripts/CompassCompile.groovy")
 eventCreateWarStart = { name, stagingDir ->
 	// someday, figure a way to just call compassPluginCompile() from CompassCompile.groovy
 	ant.path(id: 'jruby.classpath') { 
-		pathelement(location: "${compassPluginDir}/lib/jruby-complete-1.7.0.jar") 
+		pathelement(location: "${compassPluginDir}/libs/jruby-complete-1.7.0.jar") 
 	}
 	new File("web-app/themes").eachFile { file ->
 		if(file.isDirectory() && file.name.endsWith(".theme")) {
@@ -12,14 +12,12 @@ eventCreateWarStart = { name, stagingDir ->
 					fork:true, 
 					failonerror:true, 
 					classpathref: 'jruby.classpath',
-					dir: file
+					dir: new File(file.getPath() + '/sass')
 			) {
-					env(key: "GEM_HOME", value: "${compassPluginDir}/lib/gems")
+					env(key: "GEM_HOME", value: "${compassPluginDir}/libs/gems")
 					arg(value: "--1.8")
-					arg(value:"${compassPluginDir}/lib/gems/bin/compass")
+					arg(value:"${compassPluginDir}/libs/gems/bin/compass")
 					arg(value:"compile")
-					arg(value: "-c")
-					arg(value: "sass/config.rb")
 			}
 		}
 	}
