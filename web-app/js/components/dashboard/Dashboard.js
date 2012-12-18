44d407e3e434a6620d2ed7f14b684dea357cec67
Nish Patel <nishantpatel611@gmail.com>
2012-12-18 15:30:26 -0500

    OWF-7297: Error thrown when switching dashboards when first dashboard has a background widget while using Internet Explorer 9.

--- web-app/js/components/dashboard/Dashboard.js
+++ web-app/js/components/dashboard/Dashboard.js
@@ -873,7 +873,7 @@ Ext.define('Ozone.components.dashboard.Dashboard', {
             //cmp.show();
             //cmp.fireEvent('show');
             var onShow;
-            if(onShow = cmp['on_show']) {
+            if(cmp && (onShow = cmp['on_show'])) {
                 onShow();
             }
         }
@@ -892,7 +892,7 @@ Ext.define('Ozone.components.dashboard.Dashboard', {
             //cmp.hide();
             //cmp.fireEvent('hide');
             var onHide;
-            if(onHide = cmp['on_hide']) {
+            if(cmp && (onHide = cmp['on_hide'])) {
                 onHide();
             }
         }
