15f2ba617a575adf19de28e69cd9b1ea79c2b316
Lawerence Mize <lawerence.mize@nextcentury.com>
2012-12-20 12:28:03 -0500

    OWF-7309: added htmlencoding to the created by field.

--- web-app/js/components/window/DashboardSwitcher.js
+++ web-app/js/components/window/DashboardSwitcher.js
@@ -136,7 +136,7 @@ Ext.define('Ozone.components.window.DashboardSwitcher', {
                             str = str + groupStr + '</p>';
                         }
                     } 
-                    return str + '<p class=\'created-by\'><label>Created by: </label>' + (values.createdBy.userRealName || '') + '</p>' +
+                    return str + '<p class=\'created-by\'><label>Created by: </label>' + (Ext.htmlEncode(Ext.htmlEncode(values.createdBy.userRealName)) || '') + '</p>' +
                            '<p class=\'last-updated\'><label>Last Modified: </label>' + (values.prettyEditedDate || '') + '</p></div>"';
                 }
             },
