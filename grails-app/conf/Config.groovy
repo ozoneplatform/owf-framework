d9016604910165afeaa192c297a7ee9d219e4612
Ross Pokorny <ross.pokorny@nextcentury.com>
2013-01-21 12:56:57 -0500

    OWF-7485: Modifying perf test to trigger error

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -798,22 +798,22 @@ environments {
     development {
         uiperformance.enabled = false
 
-        perfTest.enabled = false            // Determines whether or not bootstrap is run
+        perfTest.enabled = true            // Determines whether or not bootstrap is run
         perfTest.clearCacheEvery = 10
-        perfTest.numDashboards = 5          // The number of dashboards each user will get assigned to them
+        perfTest.numDashboards = 0          // The number of dashboards each user will get assigned to them
         perfTest.numGroupDashboards = 1     // The number of dashboards each group will get assigned to them
         perfTest.numDashboardsWidgets = 5   // The number of widgets on each dashboard
         perfTest.numAdmins = 5              // The number of administrators
-        perfTest.numUsers = 995             // The number of non-admin users
-        perfTest.numGroups = 50             // The number of groups
+        perfTest.numUsers = 1500             // The number of non-admin users
+        perfTest.numGroups = 1             // The number of groups
         perfTest.numGroupsPerUser = 1       // The number of groups each user is assigned to
         perfTest.numWidgetsInGroups = 0     // The number of widgets assigned to each group
         perfTest.numWidgetsPerUser = 5      // The number of widgets assigned to each user
-        perfTest.numPreferences = 50        // The number of preferences per user
-        perfTest.numWidgets = 500           // The number of unique widget definitions
-        perfTest.numStacks = 5              // The number of stacks
-        perfTest.numStacksPerUser = 2       // The number of stacks per user
-        perfTest.numStackDashboards = 2     // The number of dashboards each stack will get assigned to them
+        perfTest.numPreferences = 0        // The number of preferences per user
+        perfTest.numWidgets = 10           // The number of unique widget definitions
+        perfTest.numStacks = 0              // The number of stacks
+        perfTest.numStacksPerUser = 0       // The number of stacks per user
+        perfTest.numStackDashboards = 0     // The number of dashboards each stack will get assigned to them
 
         perfTest.createSampleWidgets = false
         perfTest.sampleWidgetBaseUrl = 'https://127.0.0.1:8443/owf/sampleWidgets/'
