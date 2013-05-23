b49f97b0795932e0cd403ea1525ca2d74a45a67d
Andrew Curry <andrew.curry@nextcentury.com>
2013-05-23 15:46:22 -0400

    OP-309 (OP-205) - Fix for error when adding widget from Apps Mall to OWF.

--- test/unit/ozone/owf/grails/services/MarketplaceServiceTests.groovy
+++ test/unit/ozone/owf/grails/services/MarketplaceServiceTests.groovy
@@ -1,12 +1,11 @@
 package ozone.owf.grails.services
 
-import grails.test.*
-
+import grails.converters.deep.JSON
+import grails.test.GrailsUnitTestCase
 import org.codehaus.groovy.grails.web.json.JSONArray
-
+import org.json.JSONString
+import ozone.owf.grails.OwfException
 import ozone.owf.grails.domain.*
-import grails.converters.JSON
-import org.springframework.context.ApplicationContext
 
 class MarketplaceServiceTests extends GrailsUnitTestCase {
     MarketplaceService marketplaceService
@@ -16,9 +15,9 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
     protected void setUp() {
         super.setUp()
         marketplaceService = new MarketplaceService()
-        def domainMappingServiceMockClass= mockFor(DomainMappingService)
-        domainMappingServiceMockClass.demand.deleteAllMappings(0..9999) { a,b,c -> [] }
-        marketplaceService.domainMappingService= domainMappingServiceMockClass.createMock()
+        def domainMappingServiceMockClass = mockFor(DomainMappingService)
+        domainMappingServiceMockClass.demand.deleteAllMappings(0..9999) { a, b, c -> [] }
+        marketplaceService.domainMappingService = domainMappingServiceMockClass.createMock()
 
         stackServiceMockClass = mockFor(StackService)
         marketplaceService.stackService = stackServiceMockClass.createMock()
@@ -26,23 +25,22 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
         widgetDefinitionServiceMockClass = mockFor(WidgetDefinitionService)
         marketplaceService.widgetDefinitionService = widgetDefinitionServiceMockClass.createMock()
 
-
         // Stub out taggable.  Kinda annoying
         WidgetDefinition.metaClass.mockTags = []
-        WidgetDefinition.metaClass.getTags= { 
-            return delegate.mockTags.collect {[tag:[name:it]]}
+        WidgetDefinition.metaClass.getTags = {
+            return delegate.mockTags.collect { [tag: [name: it]] }
         }
-        WidgetDefinition.metaClass.removeTag={ name -> delegate.mockTags.remove(name)}
-        WidgetDefinition.metaClass.addTag={name, visible, position, editable -> delegate.mockTags << name}
-        
+        WidgetDefinition.metaClass.removeTag = { name -> delegate.mockTags.remove(name) }
+        WidgetDefinition.metaClass.addTag = { name, visible, position, editable -> delegate.mockTags << name }
+
         // Mock out the various widget types available
-        mockDomain(WidgetType,[
-            new WidgetType(name:"standard"),
-            new WidgetType(name:"marketplace"),
-            new WidgetType(name:"metric"),
-            new WidgetType(name:"administration"),
+        mockDomain(WidgetType, [
+                new WidgetType(name: "standard"),
+                new WidgetType(name: "marketplace"),
+                new WidgetType(name: "metric"),
+                new WidgetType(name: "administration"),
         ])
-        
+
         mockDomain(WidgetDefinitionIntent)
         mockDomain(Intent)
         mockDomain(IntentDataType)
@@ -73,6 +71,29 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
         "widgetTypes":["standard"]
     }
     '''
+
+    def singleSimpleWidgetJsonWithUniversalNameJson='''
+    {
+        "displayName":"name",
+        "description":"description",
+        "imageUrlLarge":"largeImage",
+        "imageUrlSmall":"smallImage",
+        "widgetGuid":"086ca7a6-5c53-438c-99f2-f7820638fc6f",
+        "widgetUrl":"http://wikipedia.com",
+        "widgetVersion":"1",
+        "singleton":false,
+        "visible":true,
+        "background":false,
+        "height":200,
+        "width":300,
+        "directRequired" :[],
+        "defaultTags" : ["tag"],
+        "intents":{"send":[],"receive":[]},
+        "widgetTypes":["standard"],
+        "universalName":"universalName"
+    }
+    '''
+
     def singleWidgetWithIntentsJson='''
     {
         "displayName":"nameIntents",
@@ -124,6 +145,7 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
         "widgetTypes":["administration", "marketplace", "metric", "nonexistent"]
     }
     '''
+
     def singleSimpleStackJson='''
     {
         "name": "Me",
@@ -151,6 +173,7 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
     '''
 
     def singleSimpleWidget=new JSONArray("[${singleSimpleWidgetJson}]")
+    def singleSimpleWidgetWithUniversalName=new JSONArray("[${singleSimpleWidgetJsonWithUniversalNameJson}]")
     def singleWidgetWithIntents=new JSONArray("[${singleWidgetWithIntentsJson}]")
     def withAndWithoutIntents=new JSONArray("[${singleSimpleWidgetJson},${singleWidgetWithIntentsJson}]")
     def widgetWithInterestingWidgetTypes = new JSONArray("[${widgetWithInterestingWidgetTypesJSON}]")
@@ -158,11 +181,11 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
 
     // just make sure that it actually parses a basic widget
     void testSimplestWidget() {
-        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..1) { a,b -> true}
+        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..1) { a, b -> true }
 
         def widgets = marketplaceService.addListingsToDatabase(singleSimpleWidget);
         def resultWidget = widgets[0]
-        assert 1,widgets.size()
+        assert 1, widgets.size()
         assert "name" == resultWidget.displayName
         assert "description" == resultWidget.description
         assert "largeImage" == resultWidget.imageUrlLarge
@@ -182,28 +205,35 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
         assert 'standard' == typesList[0].name
     }
 
+    void testSimplestWidgetWithExistingUniversalNameFails() {
+        shouldFail(OwfException) {
+            widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..1) { a, b -> false }
+            marketplaceService.addListingsToDatabase(singleSimpleWidgetWithUniversalName)
+        }
+    }
+
     void testProcessesIntentsOnOneWidget() {
-        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..1) { a,b -> true}
+        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..1) { a, b -> true }
 
         def resultWidget = marketplaceService.addListingsToDatabase(singleWidgetWithIntents)[0];
 
-        assertEquals 2,resultWidget.widgetDefinitionIntents.size()
-        boolean hasSend=false,hasReceive=false
+        assertEquals 2, resultWidget.widgetDefinitionIntents.size()
+        boolean hasSend = false, hasReceive = false
         resultWidget.widgetDefinitionIntents.each {
-            if(it.intent.action == "send") {
-                hasSend=true
-            } else if(it.intent.action == "receive") {
-                hasReceive=true
+            if (it.intent.action == "send") {
+                hasSend = true
+            } else if (it.intent.action == "receive") {
+                hasReceive = true
             } else {
                 fail("Should not have action: " + it.intent.action)
             }
-            assertEquals "Should be two data types for this action",2,it.dataTypes.size()
-            assertTrue "Should have the text/plain datatype", it.dataTypes.find { it.dataType == "text/plain"} != null
-            assertTrue "Should have the text/plain datatype", it.dataTypes.find { it.dataType == "text/html"} != null
+            assertEquals "Should be two data types for this action", 2, it.dataTypes.size()
+            assertTrue "Should have the text/plain datatype", it.dataTypes.find { it.dataType == "text/plain" } != null
+            assertTrue "Should have the text/plain datatype", it.dataTypes.find { it.dataType == "text/html" } != null
 
         }
-        assertTrue "Definition should contain send intents",hasSend
-        assertTrue "Definition should contain receive intents",hasReceive
+        assertTrue "Definition should contain send intents", hasSend
+        assertTrue "Definition should contain receive intents", hasReceive
 
         def typesList = resultWidget.widgetTypes as List
 
@@ -212,22 +242,22 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
     }
 
     void testMultipleWidgets() {
-        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..2) { a,b -> true}
+        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..2) { a, b -> true }
 
-        def widgets=marketplaceService.addListingsToDatabase(withAndWithoutIntents);
-        assertEquals 2,widgets.size()
-        assertEquals "name",widgets[0].displayName
-        assertEquals "nameIntents",widgets[1].displayName
+        def widgets = marketplaceService.addListingsToDatabase(withAndWithoutIntents);
+        assertEquals 2, widgets.size()
+        assertEquals "name", widgets[0].displayName
+        assertEquals "nameIntents", widgets[1].displayName
     }
 
     void testAllWidgetTypesProperlyConverted() {
-        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..1) { a,b -> true}
+        widgetDefinitionServiceMockClass.demand.canUseUniversalName(0..1) { a, b -> true }
 
         def resultWidget = marketplaceService.addListingsToDatabase(widgetWithInterestingWidgetTypes)[0];
 
-        def typeNamesList = ((resultWidget.widgetTypes as List).sort{WidgetType type -> type.name})*.name
+        def typeNamesList = ((resultWidget.widgetTypes as List).sort { WidgetType type -> type.name })*.name
 
-        println (typeNamesList.join(', '))
+        println(typeNamesList.join(', '))
 
         assert 4 == typeNamesList.size()
         assert ['administration', 'marketplace', 'metric', 'standard']
@@ -235,8 +265,8 @@ class MarketplaceServiceTests extends GrailsUnitTestCase {
 
     // just make sure that it recognizes a stack and passes it to the StackService
     void testSimplestStack() {
-        stackServiceMockClass.demand.importStack{params ->
-            assertEquals([ data: singleSimpleStack[0].toString()], params)
+        stackServiceMockClass.demand.importStack { params ->
+            assertEquals([data: singleSimpleStack[0].toString()], params)
         }
 
         marketplaceService.addListingsToDatabase(singleSimpleStack);
