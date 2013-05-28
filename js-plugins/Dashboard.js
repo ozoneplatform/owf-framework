62c385fbba33c8e5741da3fea49cba8d79bb74cb
Tina Coleman <tina.coleman@nextcentury.com>
2013-05-28 19:17:11 -0400

    OP-23, subtask OP-590: add getIsModifiable to Dashboard component.  Also see use of feature flag.

--- js-plugins/Dashboard.js
+++ js-plugins/Dashboard.js
@@ -4,6 +4,10 @@ Ext.define('Ozone.plugins.Dashboard', {
 	init: function(cmp) {
 		
 		// console.log('Dashboard external plugin');
+        //if  (Ozone.config.debugFeatureFlags && Ozone.config.debugFeatureFlags.OP23 && Ozone.config.debugFeatureFlags.OP23 === true) {
+            console.log("Modifiable..." + cmp.getIsModifiable());
+        //}
+
 
 	}
 });
