423a15c2a91cd8566a6ebc2945f589a4e838888e
Rob Szewczyk <rob.szewczyk@nextcentury.com>
2013-06-18 13:38:11 -0400

    Removing minor version number from current release. Adding the help column to the changelogs for the application configuration table

--- grails-app/migrations/changelog_7.3.groovy
+++ grails-app/migrations/changelog_7.3.groovy
@@ -1,12 +1,12 @@
 databaseChangeLog = {
-    changeSet(author: "owf", id: "7.3.0-1", context: "create, upgrade, 7.3.0") {
+    changeSet(author: "owf", id: "7.3-1", context: "create, upgrade, 7.3") {
         comment("Add type to dashboard")
         addColumn(tableName: "dashboard") {
             column(name: "type", type: "varchar(255)")
         }
     }
 
-    changeSet(author: "owf", id: "7.3.0-2", context: "upgrade, 7.3.0, sampleData, 7.3.0-sampleData") {
+    changeSet(author: "owf", id: "7.3-2", context: "upgrade, 7.3, sampleData, 7.3-sampleData") {
         comment("Update existing dashboards to set type to marketplace if name is Apps Mall")
         update(tableName: "dashboard") {
             column(name: "type", value: 'marketplace')
@@ -15,7 +15,7 @@ databaseChangeLog = {
     }
 	
 		
-	changeSet(author: "owf", dbms: "mysql, oracle, postgresql", id: "7.3.0-3", context: "create, upgrade, 7.3.0") {
+	changeSet(author: "owf", dbms: "mysql, oracle, postgresql", id: "7.3-3", context: "create, upgrade, 7.3") {
 		createTable(tableName: "application_configuration") {
 
 			column(autoIncrement: "true", name: "id", type: "java.sql.Types.BIGINT") {
@@ -67,12 +67,14 @@ databaseChangeLog = {
 			}
 			
 			column(name: "sub_group_order", type: "java.sql.Types.BIGINT")
+
+            column(name: "help", type: "java.sql.Types.VARCHAR(2000)")
 						
 		}
 
 	}
 
-	changeSet(author: "owf", dbms: "mssql", id: "7.3.0-3", context: "create, upgrade, 7.3.0") {
+	changeSet(author: "owf", dbms: "mssql", id: "7.3-3", context: "create, upgrade, 7.3") {
 		createTable(tableName: "application_configuration") {
 
 			column(autoIncrement: "true", name: "id", type: "java.sql.Types.BIGINT") {
@@ -127,7 +129,7 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "owf", id: "7.3.0-4", context: "create, upgrade, 7.3.0") {
+	changeSet(author: "owf", id: "7.3-4", context: "create, upgrade, 7.3") {
 		createIndex(indexName: "FKFC9C0477666C6D2", tableName: "application_configuration") {
 			column(name: "created_by_id")
 		}
@@ -144,5 +146,5 @@ databaseChangeLog = {
 		addForeignKeyConstraint(baseColumnNames: "created_by_id", baseTableName: "application_configuration", constraintName: "FKFC9C0477666C6D2", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "person", referencesUniqueColumn: "false")
 		addForeignKeyConstraint(baseColumnNames: "edited_by_id", baseTableName: "application_configuration", constraintName: "FKFC9C047E31CB353", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "person", referencesUniqueColumn: "false")
 	}
-	
+
 }
