476322d91c231b131ce840aee043bbc1b54479a0
Jeremy Hammond <jeremy.hammond@nextcentury.com>
2013-06-18 19:10:41 -0400

    OP-771 added workflow to app config controller

--- grails-app/controllers/ozone/owf/grails/controllers/ApplicationConfigurationController.groovy
+++ grails-app/controllers/ozone/owf/grails/controllers/ApplicationConfigurationController.groovy
@@ -1,15 +1,28 @@
 package ozone.owf.grails.controllers
 import grails.converters.JSON
 import org.ozoneplatform.appconfig.client.controller.AbstractApplicationConfigurationController
+import org.ozoneplatform.appconfig.server.domain.model.ApplicationConfiguration;
+
 import ozone.owf.grails.services.OwfApplicationConfigurationService
 
 
 class ApplicationConfigurationController extends AbstractApplicationConfigurationController{
 
+	OwfApplicationConfigurationService owfApplicationConfigurationService
 	
 	@Override
 	void doRender(def model){
 		render model as JSON
+	}
+
+	@Override
+	public void doSave(ApplicationConfiguration applicationConfiguration) {
+		owfApplicationConfigurationService.saveApplicationConfiguration(applicationConfiguration)
+	}
+
+	@Override
+	public void doValidate(ApplicationConfiguration applicationConfiguration) {
+		owfApplicationConfigurationService.validate(applicationConfiguration)
 	}
 	
 }
