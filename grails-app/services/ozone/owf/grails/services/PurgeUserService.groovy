3fd181598949dc137caeaa8b9404bf07e443d918
Tom DeHart <tdehart@gmail.com>
2013-06-27 12:27:07 -0400

    OP-727: fix for purge user service constraint error

--- grails-app/services/ozone/owf/grails/services/PurgeUserService.groovy
+++ grails-app/services/ozone/owf/grails/services/PurgeUserService.groovy
@@ -1,6 +1,7 @@
 package ozone.owf.grails.services
 
 import ozone.owf.grails.domain.Person
+import ozone.owf.grails.domain.Dashboard
 import ozone.owf.enums.OwfApplicationSetting
 
 class PurgeUserService {
@@ -24,6 +25,33 @@ class PurgeUserService {
     }
 
     void purgeUser(Person person) {
+        //we need to make a copy of the list of groups because hibernate will update the same list when a the user is
+        //removed from the group
+        def groups = person.groups.collect{it}
+        groups.each { it.removeFromPeople(person) }
+
+        //we need to unset audit log fields which will
+        //cause a constraint violation if we let grails simply cascade delete the user
+
+        //search through all dashboards for dashboards which have reference to the user being deleted
+        def dashboards = Dashboard.withCriteria { eq('createdBy',person) }
+
+        //if there are any dashboards unassign this user from the fields
+        dashboards.each {
+            //explicitly clear those audit log fields
+            it.createdBy = null
+        }
+
+        //search through all dashboards for dashboards which have reference to the user being deleted
+        dashboards = Dashboard.withCriteria { eq('editedBy',person) }
+
+        //if there are any dashboards unassign this user from the fields
+        dashboards.each {
+            //explicitly clear those audit log fields
+            it.editedBy = null
+        }
+
+        //delete person
         person.delete()
     }
 }
