f9bb6d892ffa3ff99ac791ccfdeff88c3d58c02f
Jeremy Hammond <jeremy.hammond@nextcentury.com>
2013-07-10 09:56:16 -0400

    OP-770 implemented job to sweep files for owf

---
+++ grails-app/jobs/ozone/owf/grails/jobs/OwfMoveCefLogsJob.groovy
@@ -0,0 +1,40 @@
+package ozone.owf.grails.jobs
+
+import org.ozoneplatform.auditing.quartz.jobs.AbstractFileMovementJob
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import static ozone.owf.enums.OwfApplicationSetting.*
+import ozone.owf.grails.services.OwfApplicationConfigurationService;
+
+
+
+class OwfMoveCefLogsJob extends  AbstractFileMovementJob{
+	
+	OwfApplicationConfigurationService owfApplicationConfigurationService
+	
+	def group = "owfFileMoverGroup"
+	
+	
+	static triggers = { 
+		long fiveMinutes = 1000 * 60 * 5
+		long oneHour = fiveMinutes * 12
+		simple name: 'OwfMoveCefLogsTrigger', startDelay: fiveMinutes, repeatInterval: oneHour
+	}
+	
+	@Override
+	public String getFromLocation(){
+		owfApplicationConfigurationService.valueOf(CEF_LOG_LOCATION)
+	}
+	
+	@Override
+	public String getToLocation(){
+		owfApplicationConfigurationService.valueOf(CEF_LOG_SWEEP_LOCATION)
+	}
+	
+	@Override
+	public boolean isJobEnabled(){
+		owfApplicationConfigurationService.is(CEF_LOG_SWEEP_ENABLED)
+	}
+	
+}
