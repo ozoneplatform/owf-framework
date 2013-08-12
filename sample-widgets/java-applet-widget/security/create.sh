0f5d160f08343f1b77099140c65a2b0a5f2e6f9c
Andrew Curry <andrew.curry@nextcentury.com>
2013-08-12 10:20:06 -0400

    Changed create.sh keypass from 123456 to 'changeit'.

--- sample-widgets/java-applet-widget/security/create.sh
+++ sample-widgets/java-applet-widget/security/create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/keytool -genkey -alias owf -keyalg RSA -keysize 1024 -dname "CN=testUser1,O=Ozone3,ST=Maryland,C=US" -keypass 123456 -keystore ../keystore.jks -storepass 123456
+/usr/bin/keytool -genkey -alias owf -keyalg RSA -keysize 1024 -dname "CN=testUser1,O=Ozone3,ST=Maryland,C=US" -keypass changeit -keystore ../keystore.jks -storepass changeit
