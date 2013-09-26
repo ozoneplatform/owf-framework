fd533ad2f25f8522bd91c3578eca657d4f9de9ce
Rob Szewczyk <rob.szew@gmail.com>
2013-09-26 16:09:25 -0400

    OP-3287: Removed the free text warning entry from OwfConfig.groovy

--- src/resources/OwfConfig.groovy
+++ src/resources/OwfConfig.groovy
@@ -43,11 +43,6 @@ owf {
     showAccessAlert = "true"
     accessAlertMsg = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum eleifend sapien dignissim malesuada. Sed imperdiet augue vitae justo feugiat eget porta est blandit. Proin ipsum ipsum, rutrum ac gravida in, ullamcorper a augue. Sed at scelerisque augue. Morbi scelerisque gravida sapien ut feugiat. Donec dictum, nisl commodo dapibus pellentesque, enim quam consectetur quam, at dictum dui augue at risus. Ut id nunc in justo molestie semper. Curabitur magna velit, varius eu porttitor et, tempor pulvinar nulla. Nam at tellus nec felis tincidunt fringilla. Nunc nisi sem, egestas ut consequat eget, luctus et nisi. Nulla et lorem odio, vitae pretium ipsum. Integer tellus libero, molestie a feugiat a, imperdiet sit amet metus. Aenean auctor fringilla eros, sit amet suscipit felis eleifend a."
 
-    // Specifies a freeTextEntryMessage to appear on all dialogs which allow text entry
-    // To turn off the warning message, use the following:
-    //     freeTextEntryWarningMessage=''
-    freeTextEntryWarningMessage=''
-
     //use to specify a logout url
     logoutURL = "/logout"
 
