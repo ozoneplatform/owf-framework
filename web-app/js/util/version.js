a82c3964a1b5eb3c0ec505e4cb2f826d92d94772
Neil Castine <neil.castine@nextcentury.com>
2013-12-23 19:43:18 -0500

    OP-4208: Update version numbers

--- web-app/js/util/version.js
+++ web-app/js/util/version.js
@@ -2,25 +2,25 @@ var Ozone = Ozone || {};
 Ozone.version = Ozone.version || {};
 
 Ozone.version = {
-		
-		owfversion : '7.1.0-ALPHA-SPRINT1',
 
-        mpversion : '2.3',
+    owfversion: '7.0.1-GA',
 
-		preference : '-v1',
-		
-		eventing : '-v1',
-		
-		widgetLauncher : '-v1',
-		
-		state : '-v1',
-		
-        dragAndDrop : '-v1',
+    mpversion: '2.3',
 
-        widgetChrome : '-v1',
+    preference: '-v1',
 
-		logging : '-v1',
-		
-		language : '-v1'
-		
+    eventing: '-v1',
+
+    widgetLauncher: '-v1',
+
+    state: '-v1',
+
+    dragAndDrop: '-v1',
+
+    widgetChrome: '-v1',
+
+    logging: '-v1',
+
+    language: '-v1'
+
 };
