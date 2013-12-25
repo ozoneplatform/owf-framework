755361db6703e0fa60a7d2e133d7ee6de2b19822
Neil Castine <neil.castine@nextcentury.com>
2013-12-25 00:06:42 -0500

    OP-4246 Update version numbers

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -5,12 +5,13 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 databaseChangeLog = {
 
     //previous version change logs go here
-	include file: 'changelog_3.7.0.groovy'
-	include file: 'changelog_3.8.0.groovy'
+    include file: 'changelog_3.7.0.groovy'
+    include file: 'changelog_3.8.0.groovy'
     include file: 'changelog_3.8.1.groovy'
     include file: 'changelog_4.0.0.groovy'
     include file: 'changelog_5.0.0.groovy'
     include file: 'changelog_6.0.0.groovy'
+    include file: 'changelog_6.0.1.groovy'
 
     //include/exclude current version's change log based on existence of dbmBuildPreviousVersion
     if (!System.properties.dbmBuildPreviousVersion) {
