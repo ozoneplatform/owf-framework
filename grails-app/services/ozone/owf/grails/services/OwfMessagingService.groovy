688358d8026ad06b21aafe7499415053b38164c2
Jeremy Hammond <jeremy.hammond@nextcentury.com>
2014-01-15 10:25:17 -0500

    OP-4306 changed logging from console to log4j

--- grails-app/services/ozone/owf/grails/services/OwfMessagingService.groovy
+++ grails-app/services/ozone/owf/grails/services/OwfMessagingService.groovy
@@ -1,17 +1,17 @@
 package ozone.owf.grails.services
 
 import org.ozoneplatform.messaging.service.api.MessageService
-
+import org.ozoneplatform.messaging.payload.AmlMessage
 class OwfMessagingService {
 
     MessageService messageService
     
     
-    public void listen(){
+    public AmlMessage listen(){
         //For now this is invoked in bootstrap but in reality this will be from a controller
         messageService.listenForMessage("amlnotify_owfuser", "amlnotify@conference.goss.owfgoss.org", { message ->
             log.info "Message recieved.  The contents are: ${message}."
+            return message
         });
-    
     }
 }
