9b585a601f16804099d4c29fdcdd86098e13b782
Jeremy Hammond <jeremy.hammond@nextcentury.com>
2014-01-24 10:55:13 -0500

    OP-4428 initial seeding with MessagesController

---
+++ src/groovy/ozone/owf/cache/OwfMessageCache.groovy
@@ -0,0 +1,12 @@
+package ozone.owf.cache
+
+import org.ozoneplatform.messaging.payload.AmlMessage
+
+class OwfMessageCache {
+
+    
+    
+    public void add(AmlMessage message){
+        
+    }
+}
