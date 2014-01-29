c032e0a29c5cc8873c262a610184df06bfdfde72
Jeremy Hammond <jeremy.hammond@nextcentury.com>
2014-01-29 14:45:51 -0500

    OP-4428 fixed column type for person.last_notification

--- grails-app/migrations/changelog_7.10.0.groovy
+++ grails-app/migrations/changelog_7.10.0.groovy
@@ -2,7 +2,9 @@ databaseChangeLog = {
     
     changeSet(author: "owf", id: "7.10.0-1", dbms: "mysql,postgresql,oracle,hsqldb", context: "create, upgrade, 7.10.0") {
         addColumn(tableName: "person") {
-            column(name: "last_notification", type: "timestamp")
+            column(name: "last_notification", type: "timestamp"){
+                constraints(nullable: "true")
+            }
         }
     }
 
