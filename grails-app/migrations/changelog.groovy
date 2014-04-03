7436adb17d17ff3e23eaad1621bbcf3b4c4abdc5
Rob Szewczyk <rob.szew@gmail.com>
2014-04-03 12:42:10 -0400

    OP-5241: Version Bump

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -37,6 +37,7 @@ databaseChangeLog = {
     include file: 'changelog_7.11.0.groovy'
     include file: 'changelog_7.12.0.groovy'
     include file: 'changelog_7.13.0.groovy'
+    include file: 'changelog_7.14.0.groovy'
 
     //include/exclude current version's change log based on existence of dbmBuildPreviousVersion
     if (!System.properties.dbmBuildPreviousVersion) {
