3e11c187a2370de51a04bd6c62d328c77b61b135
Ross Pokorny <ross.pokorny@nextcentury.com>
2014-05-28 09:18:32 -0400

    OP-5634: Fixing sql server script

--- src/assemble/dbscripts/SQLServerPrefsUpgrade_v7.0.1_v7.15.1.sql
+++ src/assemble/dbscripts/SQLServerPrefsUpgrade_v7.0.1_v7.15.1.sql
@@ -1,4 +1,3 @@
--- No Changes from 7.0.0 to 7.0.1
 -- No Changes from 7.0.0 to 7.1.0-- *********************************************************************
 -- Update Database Script
 -- *********************************************************************
@@ -20,52 +19,6 @@ SET IDENTITY_INSERT [dbo].[widget_type] OFF
 GO
 
 INSERT INTO [dbo].[DATABASECHANGELOG] ([AUTHOR], [COMMENTS], [DATEEXECUTED], [DESCRIPTION], [EXECTYPE], [FILENAME], [ID], [LIQUIBASE], [MD5SUM], [ORDEREXECUTED]) VALUES ('owf', 'Add fullscreen widget types to table. For SQL server we need to explicitly allow inserting into an auto increment field', GETDATE(), 'Custom SQL, Insert Row, Custom SQL', 'EXECUTED', 'changelog_7.2.0.groovy', '7.2.0-1', '2.0.1', '3:f87c5ac9001012f87a9854abfd7b4161', 65)
-GO
-
--- Release Database Lock
--- *********************************************************************
--- Update Database Script
--- *********************************************************************
--- Change Log: changelog.groovy
--- Ran at: 5/23/14 10:38 AM
-
--- Liquibase version: 2.0.1
--- *********************************************************************
-
--- Lock Database
--- Changeset changelog_7.15.1.groovy::7.15.1-1::owf::(Checksum: 3:a253a2a9c7e7571b94e59fc1767c58b3)
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'notifications.xmpp.server.hostname'
-GO
-
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'notifications.xmpp.server.port'
-GO
-
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'notifications.xmpp.room'
-GO
-
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'notifications.xmpp.username'
-GO
-
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'notifications.xmpp.password'
-GO
-
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'notifications.enabled'
-GO
-
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'notifications.query.interval'
-GO
-
-DELETE FROM [dbo].[application_configuration]  WHERE code = 'url.public'
-GO
-
-INSERT INTO [dbo].[DATABASECHANGELOG] ([AUTHOR], [COMMENTS], [DATEEXECUTED], [DESCRIPTION], [EXECTYPE], [FILENAME], [ID], [LIQUIBASE], [MD5SUM], [ORDEREXECUTED]) VALUES ('owf', '', GETDATE(), 'Delete Data (x8)', 'EXECUTED', 'changelog_7.15.1.groovy', '7.15.1-1', '2.0.1', '3:a253a2a9c7e7571b94e59fc1767c58b3', 1)
-GO
-
--- Changeset changelog_7.15.1.groovy::7.15.1-2::owf::(Checksum: 3:1234ac8c0f21a1d748e17510d1c4373c)
-exec sp_rename '[dbo].[widget_definition].[image_url_large]', 'image_url_medium'
-GO
-
-INSERT INTO [dbo].[DATABASECHANGELOG] ([AUTHOR], [COMMENTS], [DATEEXECUTED], [DESCRIPTION], [EXECTYPE], [FILENAME], [ID], [LIQUIBASE], [MD5SUM], [ORDEREXECUTED]) VALUES ('owf', '', GETDATE(), 'Rename Column', 'EXECUTED', 'changelog_7.15.1.groovy', '7.15.1-2', '2.0.1', '3:1234ac8c0f21a1d748e17510d1c4373c', 2)
 GO
 
 -- Release Database Lock
@@ -487,7 +440,7 @@ INSERT INTO [dbo].[DATABASECHANGELOG] ([AUTHOR], [COMMENTS], [DATEEXECUTED], [DE
 GO
 
 -- Changeset changelog_7.10.0.groovy::7.10.0-2::owf::(Checksum: 3:41ac759cfb732888d39c704edd1aa12d)
-INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.enabled', 'NOTIFICATIONS', 1, 1, ' ', 'Boolean', 'false', 0);
+INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.enabled', 'NOTIFICATIONS', 1, 1, ' ', 'Boolean', 'false', 0)
 GO
 
 INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.query.interval', 'NOTIFICATIONS', 1, 2, ' ', 'Integer', '30', 0)
