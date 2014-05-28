cf4d666cdc7ac752202e0ac9731cf5e64ffa0bc4
Ross Pokorny <ross.pokorny@nextcentury.com>
2014-05-28 11:41:33 -0400

    OP-5601: Fixing sql server script

--- src/assemble/dbscripts/SQLServerPrefsUpgrade_v7.0.1_v7.15.1.sql
+++ src/assemble/dbscripts/SQLServerPrefsUpgrade_v7.0.1_v7.15.1.sql
@@ -449,19 +449,19 @@ GO
 INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('url.public', 'NOTIFICATIONS', 1, 3, ' ', 'String', NULL, 0)
 GO
 
-INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version])VALUES ('notifications.xmpp.server.hostname', 'NOTIFICATIONS', 1, 'XMPP Settings', 1, ' ', 'String', NULL, 0)
+INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_name], [sub_group_order], [title], [type], [value], [version])VALUES ('notifications.xmpp.server.hostname', 'NOTIFICATIONS', 1, 'XMPP Settings', 1, ' ', 'String', NULL, 0)
 GO
 
-INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.server.port', 'NOTIFICATIONS', 1, 'XMPP Settings', 2, ' ', 'Integer', '5222', 0)
+INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_name], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.server.port', 'NOTIFICATIONS', 1, 'XMPP Settings', 2, ' ', 'Integer', '5222', 0)
 GO
 
-INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.room', 'NOTIFICATIONS', 1, 'XMPP Settings', 3, ' ', 'String', NULL, 0)
+INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_name], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.room', 'NOTIFICATIONS', 1, 'XMPP Settings', 3, ' ', 'String', NULL, 0)
 GO
 
-INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.username', 'NOTIFICATIONS', 1, 'XMPP Settings', 4, ' ', 'String', NULL, 0)
+INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_name], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.username', 'NOTIFICATIONS', 1, 'XMPP Settings', 4, ' ', 'String', NULL, 0)
 GO
 
-INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.password', 'NOTIFICATIONS', 1, 'XMPP Settings', 5, ' ', 'String', NULL, 0)
+INSERT INTO [dbo].[application_configuration] ([code], [group_name], [mutable], [sub_group_name], [sub_group_order], [title], [type], [value], [version]) VALUES ('notifications.xmpp.password', 'NOTIFICATIONS', 1, 'XMPP Settings', 5, ' ', 'String', NULL, 0)
 GO
 
 INSERT INTO [dbo].[DATABASECHANGELOG] ([AUTHOR], [COMMENTS], [DATEEXECUTED], [DESCRIPTION], [EXECTYPE], [FILENAME], [ID], [LIQUIBASE], [MD5SUM], [ORDEREXECUTED]) VALUES ('owf', '', GETDATE(), 'Insert Row (x8)', 'EXECUTED', 'changelog_7.10.0.groovy', '7.10.0-2', '2.0.1', '3:41ac759cfb732888d39c704edd1aa12d', 2)
