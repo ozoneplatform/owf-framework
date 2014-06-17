538191361ad28c66e63ac8da339836aa0f9bde3f
Riggs Mohler <riggs.mohler@nextcentury.com>
2014-06-17 18:15:22 -0400

    OP-5832:  Do not change 7.3 db checksums
    First, the changes in app_config_7.3.0 were reversed in order to
    preserve db checksums.
    
    Second, the changelog has been updated to appropriately assign
    cef.log.sweep and sweep.log.location

--- grails-app/migrations/changelog_7.16.0.groovy
+++ grails-app/migrations/changelog_7.16.0.groovy
@@ -1,3 +1,19 @@
 databaseChangeLog = {
-  //no changes
+    fixCefSweepConfigs = {
+        update(tableName: "application_configuration") {
+            column(name: "type", value: "String")
+            column(name: '${appconfig.valColumn}', value: "/var/log/cef")
+            where(text="code='owf.cef.sweep.log.location' AND type <> 'String'")
+        }
+
+        update(tableName: "application_configuration") {
+            column(name: "type", value: "Boolean")
+            column(name: '${appconfig.valColumn}', value: "true")
+            where(text="code='owf.enable.cef.log.sweep' AND type <> 'Boolean'")
+        }
+    }
+
+    changeSet(author: 'owf', dbms: 'mssql, mysql, postgresql, oracle, hsqldb', id: '7.16.0-1', context: 'create, upgrade, 7.16.0') {
+        fixCefSweepConfigs()
+    }
 }
