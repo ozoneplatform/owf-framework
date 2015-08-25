0b4c0c38691ba437e4edbe359e8f058880b05e6c
Stephanie Schneider <sschneider713@gmail.com>
2015-08-25 09:51:04 -0400

    Update PostgreSQL scripts, #55 contributor guidelines

--- src/assemble/dbscripts/PostgreSQLPrefsInitialCreate.sql
+++ src/assemble/dbscripts/PostgreSQLPrefsInitialCreate.sql
@@ -2,7 +2,7 @@
 -- Update Database Script
 -- *********************************************************************
 -- Change Log: changelog.groovy
--- Ran at: 8/13/15 2:02 PM
+-- Ran at: 8/24/15 8:58 AM
 -- Against: postgres@jdbc:postgresql://localhost:5432/postgres
 -- Liquibase version: 2.0.5
 -- *********************************************************************
@@ -863,4 +863,13 @@ SELECT setval('hibernate_sequence', 200);
 
 INSERT INTO databasechangelog (AUTHOR, COMMENTS, DATEEXECUTED, DESCRIPTION, EXECTYPE, FILENAME, ID, LIQUIBASE, MD5SUM, ORDEREXECUTED) VALUES ('owf', 'Updating the hibernate_sequence to account for hard coded ids', NOW(), 'Custom SQL', 'EXECUTED', 'changelog_7.16.1.groovy', '7.16.1-2', '2.0.5', '3:8698b56979b6c82e295d3f9aec41b837', 89);
 
--- Release Database Lock
+-- Changeset changelog_7.16.1.groovy::7.16.1-3::owf::(Checksum: 3:8b4c3f03d4786a6263553143cda2bde0)
+CREATE TABLE person_role (person_authorities_id BIGINT, role_id BIGINT);
+
+INSERT INTO databasechangelog (AUTHOR, COMMENTS, DATEEXECUTED, DESCRIPTION, EXECTYPE, FILENAME, ID, LIQUIBASE, MD5SUM, ORDEREXECUTED) VALUES ('owf', '', NOW(), 'Create Table', 'EXECUTED', 'changelog_7.16.1.groovy', '7.16.1-3', '2.0.5', '3:8b4c3f03d4786a6263553143cda2bde0', 90);
+
+-- Changeset changelog_7.16.1.groovy::7.16.1-4::owf::(Checksum: 3:86e4f665a39e4de4eea6cf49696b7f32)
+DROP TABLE role_people;
+
+INSERT INTO databasechangelog (AUTHOR, COMMENTS, DATEEXECUTED, DESCRIPTION, EXECTYPE, FILENAME, ID, LIQUIBASE, MD5SUM, ORDEREXECUTED) VALUES ('owf', '', NOW(), 'Drop Table', 'EXECUTED', 'changelog_7.16.1.groovy', '7.16.1-4', '2.0.5', '3:86e4f665a39e4de4eea6cf49696b7f32', 91);
+
